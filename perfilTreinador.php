8685f8c76e9f2d4d8dffcbf4292d481056ae77cb
Gabriel Klafke Teixeira <164067823+GabrielKKT@users.noreply.github.com>
2024-10-06 13:31:41 -0300

    s

--- perfilTreinador.php
+++ perfilTreinador.php
@@ -19,13 +19,15 @@ $result = $stmt->get_result();
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Perfil Treinador</title>
 </head>
 <body>
     
+
+
 </body>
 </html>
